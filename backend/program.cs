82f19ebc5b54c021c4ee5adb3b84ea81ed729338
Диана <sawdi3@icloud.com>
2026-01-28 21:50:20 +0300

    Added basic backend program

---
+++ backend/program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Приветик!");
+        Console.WriteLine("ФИО: Шумила Диана");
+        Console.WriteLine("Текущая дата и время: " + DateTime.Now);
+    }
+}
+
